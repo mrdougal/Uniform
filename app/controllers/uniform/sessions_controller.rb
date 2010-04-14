3abf062b7f7f9c0110e706479b985e5466a499ba
Dougal MacPherson <hello@newfangled.com.au>
2010-04-14 11:31:20 +1000

    Display of errors on base

--- app/controllers/uniform/sessions_controller.rb
+++ app/controllers/uniform/sessions_controller.rb
@@ -19,6 +19,8 @@ class Uniform::SessionsController < ApplicationController
       flash[:notice] = "Welcome back #{current_user.first_name}"
       redirect_back_or_default
     else
+      
+      flash[:notice] = @session.errors.on_base
       render :action => 'new'
     end
   end
@@ -46,35 +48,5 @@ class Uniform::SessionsController < ApplicationController
     session[:return_to] = nil
   end
 
-
-
-  # def flash_failure_after_create
-  #   flash.now[:failure] = translate(:bad_email_or_password,
-  #     :scope   => [:clearance, :controllers, :sessions],
-  #     :default => "Bad email or password.")
-  # end
-  # 
-  # def flash_success_after_create
-  #   flash[:success] = translate(:signed_in, :default =>  "Signed in.")
-  # end
-  # 
-  # def flash_notice_after_create
-  #   flash[:notice] = translate(:unconfirmed_email,
-  #     :scope   => [:clearance, :controllers, :sessions],
-  #     :default => "User has not confirmed email. " <<
-  #                 "Confirmation email will be resent.")
-  # end
-  # 
-  # def url_after_create
-  #   '/'
-  # end
-  # 
-  # def flash_success_after_destroy
-  #   flash[:success] = translate(:signed_out, :default =>  "Signed out.")
-  # end
-  # 
-  # def url_after_destroy
-  #   sign_in_url
-  # end
 
 end
