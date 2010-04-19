ce5baa0cfa13a36694644002066d631378435866
Dougal MacPherson <hello@newfangled.com.au>
2010-04-19 11:20:12 +1000

    Removal of commented out code

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,23 +7,3 @@ $(document).ready(function() {
 	
 });
 
-
-// $(window).scroll(function() {
-// 	
-// 	// Stuff to do when the window is scrolled
-// 	
-// 	// Find out if the note is scrolled out of view
-// 	
-// 	// $('#side .note').position({
-// 	// 	my: "center",
-// 	//   	at: "center",
-// 	//   	of: "#main"
-// 	//   });
-// 	
-// 	$('.list thead').css({
-// 		"top": $(window).scrollTop() + "px"
-// 	})
-// 	
-// });
-
-
